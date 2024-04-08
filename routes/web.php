04ae46fed9b5249527dc5940ae6fa6eb637d936c
MarvinTrey5 <santosmarvin201@gmail.com>
2024-04-07 22:11:16 -0600

    Se cambio a la página principal creada

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 // Vista de la página llamada home
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
