c6dd14329c767e3abbf9f7713369c6fa01a0f7e0
MarvinTrey5 <santosmarvin201@gmail.com>
2024-04-09 07:57:42 -0600

    Se creo la interfaz para la visualización about.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="{{asset('styles/estilosh.css')}}">
+</head>
+<body>
+        <!-- Página Principal creada por Elias / Marvin-->
+    <!-- Encabezado de la página-->
+    <header>
+        <h1>RECICLAJE WEB2024 </h1>
+        <img src="styles/imgs/recicl.jpeg">
+    </header>
+    <!-- Navegación-->
+    <nav>
+        <ul>
+            <li><a href="{{ url('/welcome') }}" target="_blank"> Pagina Base </a></li>
+            <li><a href="{{url('/')}}" target="_blank"> Home</a></li>
+        </ul>
+    </nav>
+    <!-- Container para los elementos. -->
+    <section>
+        <h2> BIENVENIDOS A LA PAGINA ABOUT <br> INFORMACIÓN SOBRE LOS CREADORES</h2>
+        <article>
+            <p>
+                1- Elías Canales Pineda SMSS124622
+                2- Marvin Javier Santos Fernández SMSS029022
+                3- 
+                4 - 
+            </p>
+        </article>
+    </section>
+    <footer>
+        <span>
+            Universidad Gerardo Barrios ☻ 2024
+        </span>
+    </footer>
+</body>
+</html>
