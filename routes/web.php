6faa33aef413e770e7057d140b7e5386095caa39
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-05 22:08:21 -0600

    Se Crearon las rutas para cada tabla/form/list

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/welcome', function () {
@@ -13,3 +13,9 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/indexUs',[UsuariosController::class,'index'])->name('indexUs');
+Route::match(['get','post'],'/guardar',[UsuariosController::class,'guardar']);
+Route::get('/editar/{id}',[UsuariosController::class,'editarformU'])->name('formeditar');
+Route::put('/editar/{id}',[UsuariosController::class,'update'])->name('update');
+Route::delete('/{id}',[UsuariosController::class, 'delete'])->name('delete');
