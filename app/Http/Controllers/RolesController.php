76b25a7a2a4556022401de98cc8877e8fdc3e3b1
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-08 10:16:54 -0600

    Se realizaron los procesos al controlador

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class RolesController extends Controller
 {
-    //
+    public function IndexR(Request $request){
+        $roles = Roles::all();
+        return view('Roles.IndexR',compact('roles'));
+    }
+    public function guardar(Request $request){
+        if($request->isMethod('GET')){
+            return view('Roles.FormGuardarR');
+        }else if($request->isMethod('POST')){
+            Roles::create($request->all());
+            return redirect('/');
+        }
+    }
+    public function EditarFormR($id){
+        $registro = Roles::findOrFail($id);
+        return view('Roles.EditarR',compact('registro'));
+    }
+    public function update(Request $request, $id){
+        $registro = Roles::findOrFail($id);
+        $registro->update($request->all());
+        return redirect('/');
+    }
+    public function delete($id){
+        $registro = Roles::findOrFail($id);
+        $registro->delete();
+        return redirect('/');
+    }
 }
