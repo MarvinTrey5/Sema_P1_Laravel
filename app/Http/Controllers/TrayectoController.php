1e4022d3f11929acce397e13d33bd5bad4aee1b9
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-08 10:17:04 -0600

    Se realizaron los procesos al controlador

--- app/Http/Controllers/TrayectoController.php
+++ app/Http/Controllers/TrayectoController.php
@@ -1,10 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\tblTrayecto;
+
+use App\Models\Trayecto;
 use Illuminate\Http\Request;
 
 class TrayectoController extends Controller
 {
-    //
+    public function IndexT(Request $request){
+        $trayecto = Trayecto::all();
+        return view('Trayecto.IndexT',compact('trayecto'));
+    }
+    public function guardar(Request $request){
+        if($request->isMethod('GET')){
+            return view('Trayecto.FormGuardarT');
+        }else if($request->isMethod('POST')){
+            Trayecto::create($request->all());
+            return redirect('/');
+        }
+    }
+    public function EditarFOrmT($id){
+        $registro = Trayecto::findOrFail($id);
+        return view('Trayecto.EditarT',compact('registro'));
+    }
+    public function update(Request $request, $id){
+        $registro = Trayecto::findOrFail($id);
+        $registro->update($request->all());
+        return redirect('/');
+    }
+    public function delete($id){
+        $registro = Trayecto::findOrFail($id);
+        $registro->delete();
+        return redirect('/');
+    }
 }
