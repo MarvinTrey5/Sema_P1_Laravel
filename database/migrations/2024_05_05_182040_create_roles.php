768b6a36547c45e14b2282dfc72273ff537c2d36
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-10 11:46:35 -0600

    Se elimino una columna id_rol porque era un increm. innesceasrio

--- database/migrations/2024_05_05_182040_create_roles.php
+++ database/migrations/2024_05_05_182040_create_roles.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->integer('id_rol');
             $table->string('nombre_rol',20);
             $table->string('descripcion',25);
         });
