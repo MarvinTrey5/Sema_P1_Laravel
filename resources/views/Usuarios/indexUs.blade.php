a66694d080c3e78f51b1935c99660cc04b712233
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-10 11:51:44 -0600

    Se realizo el diseño para la página principal Usuarios

--- resources/views/Usuarios/indexUs.blade.php
+++ resources/views/Usuarios/indexUs.blade.php
@@ -21,7 +21,7 @@
                 <a href="#">Usuarios</a>
                 <ul class="menu">
                     <li><a href="{{ url('/indexUs') }}">Listas De usuarios</a></li>
-                    <li><a href="{{ url('/guardar') }}">Agregar Usuario</a></li>
+                    <li><a href="{{ url('/guardar1') }}">Agregar Usuario</a></li>
                 </ul>
             </li>
             <li><a href="/about" target="_blank"> About </a></li>
