9227eb9d79af6a532c971a5db73d126815df81a9
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-10 22:47:39 -0600

    Se coloco el nombre de la tabla en el modelo ya que debido al nombre del Model no encontraba la tabla especificada

--- app/Models/Objetos.php
+++ app/Models/Objetos.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Objetos extends Model
 {
+    protected $table = 'objeto_basura';
     protected $fillable=[
         'id_objeto',
         'nombre_objeto'
