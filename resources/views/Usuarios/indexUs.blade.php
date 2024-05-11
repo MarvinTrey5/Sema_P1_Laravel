5135f32191aa743e04492b679c5279651bf79088
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-10 23:02:48 -0600

    Diseño para la tabla Usuarios

--- resources/views/Usuarios/indexUs.blade.php
+++ resources/views/Usuarios/indexUs.blade.php
@@ -11,7 +11,9 @@
     <!-- Encabezado de la página-->
     <header>
         <h1>RECICLAJE WEB2024 </h1>
-        <img src="styles/imgs/recicl.jpeg">
+        <a href="{{ url('/') }}">
+            <img src="/styles/imgs/recicl.jpeg">
+        </a>
     </header>
     <!-- Navegación-->
     <nav>
@@ -20,12 +22,39 @@
             <li>
                 <a href="#">Usuarios</a>
                 <ul class="menu">
-                    <li><a href="{{ url('/indexUs') }}">Listas De usuarios</a></li>
-                    <li><a href="{{ url('/guardar1') }}">Agregar Usuario</a></li>
+                    <li><a href="{{ url('/usuarios/indexUs') }}">Listas De usuarios</a></li>
+                    <li><a href="{{ url('/usuarios/guardar1') }}">Agregar Usuario</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Roles</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/roles/indexR') }}">Listas De Roles</a></li>
+                    <li><a href="{{ url('/roles/guardar2') }}">Agregar Rol</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Trayectos</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/trayectos/IndexT') }}">Listas De Trayectos</a></li>
+                    <li><a href="{{ url('/trayectos/guardarT') }}">Agregar Trayecto</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Objetos</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/objetos/IndexO') }}">Listas De Objetos</a></li>
+                    <li><a href="{{ url('/objetos/guardarR') }}">Agregar Trayecto</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Contenedores</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/contents/IndexC') }}">Listas De Contenedores</a></li>
+                    <li><a href="{{ url('/contents/guardarC') }}">Agregar Contenedores</a></li>
                 </ul>
             </li>
             <li><a href="/about" target="_blank"> About </a></li>
-
         </ul>
     </nav>
     <div class="container">
