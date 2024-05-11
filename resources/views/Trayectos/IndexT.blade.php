13c1c27d01034a0154db2beb44dc571018c5aff5
MarvinTrey5 <santosmarvin201@gmail.com>
2024-05-10 23:01:15 -0600

    Diseño para la tabla Trayectos_coleccion

--- resources/views/Trayectos/IndexT.blade.php
+++ resources/views/Trayectos/IndexT.blade.php
@@ -4,8 +4,92 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabla Principal</title>
+    <link rel="stylesheet" href="{{asset('styles/estilosh.css')}}">
 </head>
 <body>
-    
+    <!-- Página Principal creada por Elias / Marvin-->
+    <!-- Encabezado de la página-->
+    <header>
+        <h1>RECICLAJE WEB2024 </h1>
+        <a href="{{ url('/') }}">
+            <img src="/styles/imgs/recicl.jpeg">
+        </a>
+    </header>
+    <nav>
+        <ul class="horizontal">
+            <li><a href="{{ url('/welcome') }}" target="_blank"> Pagina Base </a></li>
+            <li>
+                <a href="#">Usuarios</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/usuarios/indexUs') }}">Listas De usuarios</a></li>
+                    <li><a href="{{ url('/usuarios/guardar1') }}">Agregar Usuario</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Roles</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/roles/indexR') }}">Listas De Roles</a></li>
+                    <li><a href="{{ url('/roles/guardar2') }}">Agregar Rol</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Trayectos</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/trayectos/IndexT') }}">Listas De Trayectos</a></li>
+                    <li><a href="{{ url('/trayectos/guardarT') }}">Agregar Trayecto</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Objetos</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/objetos/IndexO') }}">Listas De Objetos</a></li>
+                    <li><a href="{{ url('/objetos/guardarR') }}">Agregar Trayecto</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Contenedores</a>
+                <ul class="menu">
+                    <li><a href="{{ url('/contents/IndexC') }}">Listas De Contenedores</a></li>
+                    <li><a href="{{ url('/contents/guardarC') }}">Agregar Contenedores</a></li>
+                </ul>
+            </li>
+            <li><a href="/about" target="_blank"> About </a></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <table>
+            <caption>Lista De Trayectos</caption>
+            <thead>
+                <th>IdTrayecto</th>
+                <th>Plazo</th>
+                <th>Personal</th>
+                <th>Opciones</th>
+            </thead>
+            <tbody>
+                @foreach($trayectos as $trayecto)
+                <tr>
+                    <td>{{$trayecto->id_trayecto}}</td>
+                    <td>{{$trayecto->plazo}}</td>
+                    <td>{{$trayecto->personal}}</td>
+                    <td>
+                        <form action="{{route('formeditarT',$trayecto->id)}}" method="get">
+                            <input type="submit" value="Editar">
+                        </form>
+                        <form action="{{route('deleteT',$trayecto->id)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="Elimnar">
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+    <footer>
+        <span>
+            Universidad Gerardo Barrios ☻ 2024
+        </span>
+    </footer>
 </body>
 </html>
